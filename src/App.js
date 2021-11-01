776b9840b5002a41a6b8077211ccf7510d370182
PrAtHm3545 <prathmeshghodke3545@gmail.com>
2021-11-01 16:37:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
     </div>
   );
 }
