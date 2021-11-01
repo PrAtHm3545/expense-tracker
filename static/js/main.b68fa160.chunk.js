30764356c283107c58717576c12c3b92695167af
PrAtHm3545 <prathmeshghodke3545@gmail.com>
2021-11-01 16:33:32 +0530

    Updates

--- static/js/main.b68fa160.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpexpense-tracker"]=this["webpackJsonpexpense-tracker"]||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);var c=t(1),r=t.n(c),s=t(3),i=t.n(s),o=(t(8),t(9),t(0));var a=function(){return Object(o.jsx)("div",{className:"App",children:Object(o.jsx)("h1",{children:"Hello World"})})};i.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(a,{})}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
-//# sourceMappingURL=main.b68fa160.chunk.js.map
